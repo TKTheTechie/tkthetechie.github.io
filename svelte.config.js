6ffe7416931f73b602554c22002114f7a5db249a
TKTheTechie <thomas.kunnumpurath@solace.com>
2023-05-28 22:22:52 -0400

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import {mdsvex} from "mdsvex";
-import { fileURLToPath } from 'url';
-import * as path from 'path';
+import adapter from '@sveltejs/adapter-static';
 
-const dirname = path.resolve(fileURLToPath(import.meta.url), '../');
-
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -31,7 +28,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true,
+			paths: {
+				base: dev ? '' : process.env.BASE_PATH,
+			}
+        })
 	}
 };
 
