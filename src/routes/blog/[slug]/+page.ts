27698d361bc1e8c803eb957cb6bc6babd84175f3
TKTheTechie <thomas.kunnumpurath@solace.com>
2023-05-28 22:52:58 -0400

    Update +page.ts

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,5 +1,19 @@
 export const prerender = true;
 
+/** @type {import('./$types').EntryGenerator} */
+export function entries() {
+  const blogs = import.meta.glob('../*.md');
+
+  let slugs = [];
+
+  for (const path in blogs) {
+    slugs.push({ slug: path.substring(path.lastIndexOf("/") + 1, path.length) });
+  }
+
+  return slugs;
+}
+
+
 
 export async function load({ params }) {
   const post = await import(`../${params.slug}.md`)
