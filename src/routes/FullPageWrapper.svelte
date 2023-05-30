0b558421cd8cd9bbb469941736f65f6f29c52679
TKTheTechie <thomas.kunnumpurath@solace.com>
2023-05-30 15:08:08 -0400

    Update FullPageWrapper.svelte

--- src/routes/FullPageWrapper.svelte
+++ src/routes/FullPageWrapper.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
 	import { siteMap } from '../stores';
-	import '../lib/fullpage.parallax.min.js';
 	// @ts-ignore
-	import fullpage from '../lib/fullpage.extensions.min.js';
+	import fullpage from 'fullpage.js';
 
 	// @ts-ignore
 	let fp;
 
 	onMount(() => {
 		fp = new fullpage('#fullpage', {
-			parallaxKey: '16E9C3FF-FEDC42B5-AC45D278-D3AED2B8',
-			parallax: true,
+			// parallaxKey: '16E9C3FF-FEDC42B5-AC45D278-D3AED2B8',
+			// parallax: true,
 			anchors: Array.from(siteMap.keys()),
 			licenseKey: 'gplv3-license',
 			controlArrows: true,
