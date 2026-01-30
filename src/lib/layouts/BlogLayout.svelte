c2454b07bc997e9eaaef4cccb67def39c6e47a03
TKTheTechie <thomas.kunnumpurath@solace.com>
2026-01-29 22:29:51 -0500

    Update BlogLayout.svelte

--- src/lib/layouts/BlogLayout.svelte
+++ src/lib/layouts/BlogLayout.svelte
@@ -69,7 +69,7 @@
 
 <svelte:head>
   <title>{title} - Thomas Kunnumpurath</title>
-  <meta name="description" content="Technical blog post: {title}" />
+  <meta name="description" content={title} />
 </svelte:head>
 
 <div class="min-h-screen transition-colors duration-300" style="background: {isDark ? 'linear-gradient(to bottom right, rgb(55, 65, 81), rgb(75, 85, 99), rgb(55, 65, 81))' : 'linear-gradient(to bottom right, rgb(249, 250, 251), rgb(255, 255, 255), rgb(249, 250, 251))'}">
